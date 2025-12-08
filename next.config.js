e30467172ad53c3b0a71fa877e45ebeb68445f15
emma_s <emmaszwagiel@gmail.com>
2025-12-08 13:21:15 -0700

    Added nosniff header. Added (but commented out) STS header

--- next.config.js
+++ next.config.js
@@ -23,6 +23,23 @@ const nextConfig = {
   },
   reactStrictMode: true,
   redirects,
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          // {
+          //   key: 'Strict-Transport-Security',
+          //   value: 'max-age=63072000; includeSubDomains; preload',
+          // },
+        ],
+      },
+    ]
+  },
 }
 
 export default withPayload(nextConfig, { devBundleServerPackages: false })
