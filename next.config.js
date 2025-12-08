d07a801023eb9551bce67c954534239b61910967
emma_s <emmaszwagiel@gmail.com>
2025-12-08 14:04:12 -0700

    Added env variable for "ng_env" to enable or disable dev tools

--- next.config.js
+++ next.config.js
@@ -9,6 +9,7 @@ const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   poweredByHeader: false,
+  devIndicators: process.env.NG_ENV === 'development',
   images: {
     remotePatterns: [
       ...[NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */].map((item) => {
